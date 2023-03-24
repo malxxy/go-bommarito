9d7ddb49aea79653c6c27175acda3a97d5e09a35
malxxy <maliayraguen@gmail.com>
2023-03-24 14:17:59 -0700

    establish set up, validator npm package, home page, etc

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-router-activity',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/', // add database
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,2 +1,48 @@
-// define the schema of the database
+// define the schema of the blog database
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
+const blogSchema = new Schema({
+  blogTitle: {
+    type: String,
+    required: 'You need to insert a title!',
+    minlength: 1,
+    maxlength: 100,
+    trim: true,
+  },
+  blogText: {
+    type: String,
+    required: 'You need to insert text!',
+    minlength: 1,
+    trim: true,
+  },
+  blogAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 500,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const Blog = model('Blog', blogSchema);
+
+module.exports = Blog;
