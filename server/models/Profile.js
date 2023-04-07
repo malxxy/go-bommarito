2d00d0758c6d1ff8ab51fca95ae66b8694d2f4d6
Karinne <karinne.sagir@gmail.com>
2023-04-07 00:32:19 -0700

    added username variable to profile model

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -14,6 +14,12 @@ const profileSchema = new Schema({
     required: true,
     trim: true,
   },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
