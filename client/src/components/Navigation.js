fc7290929041d77133d3650b190900b081a2888f
Karinne <karinne.sagir@gmail.com>
2023-04-10 20:40:49 -0700

    added ApolloClient to app.js

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,48 @@
+// import React from 'react';
+
+// function Navigation({ currentPage, handlePageChange }) {
+//   return (
+   
+//     <ul className="nav nav-tabs" id="navigation" class="list">
+//     {/* <ul className=" row navigation" id="navigation"> */}
+//       <li className="nav-item">
+//         <a
+//           href="#about"
+//           onClick={() => handlePageChange('About')}
+//           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+//         >
+//           About
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#portfolio"
+//           onClick={() => handlePageChange('Portfolio')}
+//           className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Portfolio
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#contact"
+//           onClick={() => handlePageChange('Contact')}
+//           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Contact
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#resume"
+//           onClick={() => handlePageChange('Resume')}
+//           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Resume
+//         </a>
+//       </li>
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
