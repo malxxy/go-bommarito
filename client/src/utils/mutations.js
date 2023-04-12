97c544dd5c6019aa225d1420d836bab1b2374579
John Leamy <jwleamy@gmail.com>
2023-04-11 17:03:00 -0700

    comments

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client';
+/*-----All client side queries. Each exported const allows the user to manipulate the database in a certain way----- */
 
-// input requires user
+// LOGIN - requires user email and password
 export const LOGIN_USER = gql`
   mutation Login ($email: String!, $password: String!) {
   login(email: $email, password: $password) {
@@ -13,6 +14,7 @@ export const LOGIN_USER = gql`
 }
 `;
 
+// CREATE PROFILE - requires a username, email, firstname, lastname, and password
 export const ADD_PROFILE = gql`
   mutation Add_Profile ($username: String!, $email: String!, $password: String!, $firsName: String!, $lastName: String!) {
   addProfile(username: $username, email: $email, password: $password, firstName: $firsName, lastName: $lastName) {
@@ -29,6 +31,7 @@ export const ADD_PROFILE = gql`
 }
 `;
 
+// POST A NEW BLOG - requires the actual blogs' text, title, and author (author should be pulled from the active user's username)
 export const ADD_BLOG = gql`
   mutation AddBlog($blogText: String!, $blogTitle: String!, $blogAuthor: String!) {
   addBlog(blogText: $blogText, blogTitle: $blogTitle, blogAuthor: $blogAuthor) {
@@ -41,6 +44,7 @@ export const ADD_BLOG = gql`
 }
 `;
 
+// ADD COMMMENT TO BLOG POST - requires the comment text, comment author (should also be pull from active user's username), and blogID
 export const ADD_COMMENT = gql`
   mutation Comment($blogId: ID!, $commentText: String!, $commentAuthor: String!) {
   addComment(blogId: $blogId, commentText: $commentText, commentAuthor: $commentAuthor) {
@@ -51,6 +55,7 @@ export const ADD_COMMENT = gql`
 }
 `;
 
+// REMOVE BLOG POST - just requires the blog ID 
 export const REMOVE_BLOG = gql`
  mutation Mutation($blogId: ID!) {
   removeBlog(blogId: $blogId) {
@@ -60,6 +65,7 @@ export const REMOVE_BLOG = gql`
 }
 `;
 
+// REMOVE COMMENT - requires both the comment ID and the blog ID
 export const REMOVE_COMMENT = gql`
  mutation RemoveComment($blogId: ID!, $commentId: ID!) {
   removeComment(blogId: $blogId, commentId: $commentId) {
@@ -70,6 +76,7 @@ export const REMOVE_COMMENT = gql`
 }
 `;
 
+// DELETE PROFILE - requires the username and password of the profile being deleted
 export const DELETE_PROFILE = gql`
  mutation RemoveProfile($username: String!, $password: String!) {
   removeProfile(username: $username, password: $password) {
