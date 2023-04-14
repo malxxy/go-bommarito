d47bd2963df17eba16bd397cd57cbb9d89193160
malxxy <maliayraguen@gmail.com>
2023-04-13 18:15:05 -0700

    main colors to tailwind config

--- client/src/componentsAdmin/adminNav.js
+++ client/src/componentsAdmin/adminNav.js
@@ -1,1 +1,18 @@
 // admin nav from template
+import '../styles/output.css';
+
+function Nav () {
+    return (
+        <nav className="#030712 grid grid-cols-5 gap-4">
+            <div>
+                <a href="#Home">BrandImage</a>
+            </div>
+            <div className="grid-span-3">empty</div>
+            <div className="grid-span-2">
+                <a href="#Home">Home</a>
+                <a href="#Posts">Posts</a>
+                <a href="#Categories">Posts</a>
+            </div>
+        </nav>
+    )
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'mainBlue': '#243c9c',
+        'mainBlack': '#1d1d1d',
+        'mainWhite': '#f4f4f4',
+        'lightGrey': '#e8ecec',
+        'grey': '#403c3c',
+        'darkBlue': '#101b47',
+        'darkestBlue': '#040610',
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
 
