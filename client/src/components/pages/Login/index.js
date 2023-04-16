ce41457f0db29f4423aa345e5787175bd38193a8
John Leamy <jwleamy@gmail.com>
2023-04-16 13:10:00 -0700

    White screen of death gone

--- client/src/components/pages/Login/index.js
+++ client/src/components/pages/Login/index.js
@@ -1,31 +1,22 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useMutation, ApolloProvider} from '@apollo/client';
 import { LOGIN_USER } from '../../../utils/mutations';
 // import { useHistory } from 'react-router-dom';
-
 // import '../../../login-signup.css';
 import Auth from '../../../utils/auth';
-
 const Login = (props) => {
-  // const navigate = useNavigate();
-  // const [formState, setFormState] = useState({ email: '', password: '' });
-  // const [login, { error, data }] = useMutation(LOGIN_USER);
-
   const navigate = useNavigate();
-  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
-
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
       [name]: value,
     });
   };
-
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     console.log(formState);
@@ -33,21 +24,19 @@ const Login = (props) => {
       const { data } = await login({
         variables: { ...formState },
       });
-
       Auth.login(data.login.token);
 
       // Redirect the user to the homepage or another page after logging in
-      navigate('/AdminHome'); 
+      navigate('/'); 
     } catch (e) {
       console.error(e);
     }
     // clear form values
     setFormState({
-      username: '',
+      email: '',
       password: '',
     });
   };
-
   return (
     <ApolloProvider>
     <main className="flex-row justify-center mb-4 page">
@@ -55,26 +44,19 @@ const Login = (props) => {
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Login</h4>
           <div className="card-body">
-            {/* {data ? (
+            {data ? (
               <p>
                 Success! You may now head{' '}
-                <Link to="/AdminHome"> to the admin homepage.</Link>
-              </p> */}
-
-              {data && navigate('/AdminHome')} {/* Add this line to redirect the user if data is not null */}
-              <div className="col-12 col-lg-10">
-                {/* ... */}
-              </div>
-
-
-            {/* ) : ( */}
+                <Link to="/AdminHome">back to the homepage.</Link>
+              </p>
+            ) : (
               <form onSubmit={handleFormSubmit}>
                 <input
                   className="form-input"
-                  placeholder="Your username"
-                  name="username"
-                  type="username"
-                  value={formState.username}
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
                   onChange={handleChange}
                 />
                 <br></br>
@@ -95,8 +77,7 @@ const Login = (props) => {
                   Submit
                 </button>
               </form>
-            {/* )} */}
-
+            )}
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
                 {error.message}
@@ -112,5 +93,4 @@ const Login = (props) => {
     </ApolloProvider>
   );
 };
-
 export default Login;
