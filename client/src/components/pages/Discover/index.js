585e0f65842d7888ee29b32cbf6e0f3ccf3d3d74
Karinne <karinne.sagir@gmail.com>
2023-04-17 16:57:45 -0700

    updated People component

--- client/src/components/pages/Discover/index.js
+++ client/src/components/pages/Discover/index.js
@@ -97,7 +97,6 @@ const Discover = () => {
         
     return (
         <Main>
-      
               <div className="flex-row justify-center">
                 <div className="col-12 col-md-10 my-3">
                   {loading ? (
