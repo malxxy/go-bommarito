311c994511a0533f8111dcbe40b9a1c1fe055364
John Leamy <jwleamy@gmail.com>
2023-04-19 16:08:14 -0700

    Admin side

--- client/src/componentsAdmin/adminAside.js
+++ client/src/componentsAdmin/adminAside.js
@@ -7,26 +7,36 @@ function AdminAside() {
             <nav>
                 <div className="bg-mainWhite">
                     <ul>
-                        <li className="py-6 rounded-lg hover:bg-lightGrey align-middle border-t"> 
-                            <a href="Posts" className="p-4 text-lg font-semibold text-grey">
-                                Posts <FaAngleDown className="inline" />
-                            </a>
+                    <li className="py-6 rounded-lg hover:bg-lightGrey align-middle border-t"> 
+                            <Link to="/Posts">
+                                <h2 className="p-4 text-lg font-semibold text-grey">
+                                    Posts <FaAngleDown className="inline" />
+                                </h2>
+                            </Link>
                         </li>
                         <li className="py-6 rounded-lg hover:bg-lightGrey align-middle">
-                            <a href="Categories" className="p-4 text-lg font-semibold text-grey">
-                                Categories <FaAngleDown className="inline" /> </a>
+                            <Link to="/Categories">
+                                <h2 className="p-4 text-lg font-semibold text-grey">
+                                Categories <FaAngleDown className="inline" /> </h2>
+                            </Link>
                         </li>
                         <li className="py-6 rounded-lg hover:bg-lightGrey align-middle">
-                            <a href="Media" className="p-4 text-lg font-semibold text-grey">
-                                Media</a>
+                            <Link to="/Media">
+                                <h2 className="p-4 text-lg font-semibold text-grey">
+                                    Media</h2>
+                            </Link>
                         </li>
                         <li className="py-6 rounded-lg hover:bg-lightGrey align-middle">
-                            <a href="Subscribers" className="p-4 text-lg font-semibold text-grey">
-                                Subscribers <FaAngleDown className="inline" /></a>
+                            <Link to="/Subscribers">
+                                <h2 className="p-4 text-lg font-semibold text-grey">
+                                    Subscribers <FaAngleDown className="inline" /></h2>
+                            </Link>
                         </li>
                         <li className="py-6 rounded-lg hover:bg-lightGrey align-middle">
-                            <a href="Account" className="p-4 text-lg font-semibold text-grey">
-                                Account</a>
+                        <Link to="/Account">
+                            <h2 className="p-4 text-lg font-semibold text-grey">
+                                Account</h2>
+                        </Link>
                         </li>
                     </ul>
                 </div>
--- client/src/componentsAdmin/adminNav.js
+++ client/src/componentsAdmin/adminNav.js
@@ -9,20 +9,28 @@ function AdminNav () {
     return (
         <nav className="bg-mainWhite mx-auto flex items-center justify-between p-6 lg:px-8">
             <div className="flex lg:flex-1">
-                <a href="AdminHome" className="-m-1.5 p-1.5">
-                <span className="sr-only">GoBommarito</span>
-                <img className="h-12 w-auto" src='../Logo.png' alt="GoBommarito" />
-                </a>
+                <Link to='/AdminHome'>
+                    <h2 className="-m-1.5 p-1.5">
+                    <span className="sr-only">GoBommarito</span>
+                    <img className="h-12 w-auto" src='../Logo.png' alt="GoBommarito" />
+                    </h2>
+                </Link>
             </div>
             <div className="p-4">
-                <a href="AdminHome" className="p-4 text-lg rounded-md hover:bg-mainBlue hover:text-mainWhite font-semibold leading-6 text-gray-900">
-                    Home
-                </a>
-                <a href="Posts" className="p-4 text-lg  rounded-md font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
-                    Posts
-                </a>
-                <a href="Categories" className="p-4 rounded-md text-lg font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
-                    Categories</a>
+                <Link to='/AdminHome'>
+                    <h2 className="p-4 text-lg rounded-md hover:bg-mainBlue hover:text-mainWhite font-semibold leading-6 text-gray-900">
+                        Home
+                    </h2>
+                </Link>
+                <Link to='/Posts'>
+                    <h2 className="p-4 text-lg  rounded-md font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
+                        Posts
+                    </h2>
+                </Link>
+                <Link to='/Categories'>
+                    <h2 className="p-4 rounded-md text-lg font-semibold hover:bg-mainBlue hover:text-mainWhite leading-6 text-gray-900">
+                        Categories</h2>
+                </Link>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end" onClick={logout}>
                 <a href="/" className="p-4 rounded-md text-lg font-semibold leading-6 text-gray-900 hover:bg-mainBlue hover:text-mainWhite">
